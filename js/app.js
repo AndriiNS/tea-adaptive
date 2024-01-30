6a5a967e093e39832548640ed410bdf55058e330
AndriiNS <andriy.naumov05@gmail.com>
2024-01-31 00:13:20 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuClose = document.querySelector(".menu-close");
 const menuList = document.querySelector(".menu__list");
+
 menuBtn.addEventListener("click", () => {
   menuList.classList.add("menu__list--open");
+  document.body.classList.add("no-scroll");
 });
+
 menuClose.addEventListener("click", () => {
   menuList.classList.remove("menu__list--open");
+  document.body.classList.remove("no-scroll");
 });
